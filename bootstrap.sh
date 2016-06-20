3eb1da806ab198af2768de1ab5b50c7c84fada6c
Alex Corkwell <i.am.the.memory@gmail.com>
2016-06-19 23:19:42 -0700

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Bootstrap Vim.
+
+if command -v vim >/dev/null 2>&1
+then
+  # Try to install vim_bridge for python.
+  for pip in pip pip2 pip3
+  do
+    if command -v "$pip" >/dev/null 2>&1
+    then
+      "$pip" install --user vim_bridge
+    fi
+  done
+
+  # Add Vundle and clone the plugins.
+  if [ -e ~/.vim/bundle/Vundle.vim ]
+  then
+    pushd ~/.vim/bundle/Vundle.vim
+    git pull
+    popd
+  else
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  fi
+
+  vim +PluginInstall +qall
+fi
